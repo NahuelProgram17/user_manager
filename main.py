228f7f6cece2c198f6bf5c8bc02d3640231caa8a
Nahuel Pedreyra <nahuelpedreyra2017@gmail.com>
2025-12-29 09:04:53 -0300

    Agrega persistencia de usuarios en archivo JSON

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from app.user import User
+from app.storage import save_user
+
 
 def main():
-    print("=== Registro de Usuarios ===")
-
     name = input("Nombre: ")
     email = input("Email: ")
 
     try:
         user = User(name, email)
-        print("\nUsuario creado correctamente")
-        print(f"Nombre: {user.name}")
-        print(f"Email: {user.email}")
-    except ValueError as error:
-        print("\nError:", error)
+        save_user(user)
+        print("Usuario guardado correctamente ✅")
+    except ValueError as e:
+        print(e)
+
 
 if __name__ == "__main__":
     main()
